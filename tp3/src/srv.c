8184349bae4d70d638e0c00b3f14cfc080d9118d
Gabriel Marcelo Gramajo <ggramajo@ws12.labo4>
2015-06-18 21:01:32 -0300

    Comienzo de tp3, no compila

--- tp3/src/srv.c
+++ tp3/src/srv.c
@@ -11,7 +11,9 @@ void servidor(int mi_cliente)
     MPI_Status status; int origen, tag;
     int hay_pedido_local = FALSE;
     int listo_para_salir = FALSE;
-    
+    int diferidos[cant_ranks/2];
+    int respuestas_faltantes = (cant_ranks/2)-1;
+
     while( ! listo_para_salir ) {
         
         MPI_Recv(NULL, 0, MPI_INT, ANY_SOURCE, ANY_TAG, COMM_WORLD, &status);
@@ -23,8 +25,13 @@ void servidor(int mi_cliente)
             debug("Mi cliente solicita acceso exclusivo");
             assert(hay_pedido_local == FALSE);
             hay_pedido_local = TRUE;
-            debug("Dándole permiso (frutesco por ahora)");
-            MPI_Send(NULL, 0, MPI_INT, mi_cliente, TAG_OTORGADO, COMM_WORLD);
+            // debug("Dándole permiso (frutesco por ahora)");
+            // MPI_Send(NULL, 0, MPI_INT, mi_cliente, TAG_OTORGADO, COMM_WORLD);
+            for(int i = 0; i < cant_ranks; i += 2){
+                if( i != mi_rank ){
+                    MPI_Send(NULL, 0, MPI_INT, i, TAG_REQUEST, COMM_WORLD);
+                }
+            }
         }
         
         else if (tag == TAG_LIBERO) {
@@ -32,6 +39,13 @@ void servidor(int mi_cliente)
             debug("Mi cliente libera su acceso exclusivo");
             assert(hay_pedido_local == TRUE);
             hay_pedido_local = FALSE;
+            for(int i = 0; i < cant_ranks/2; ++i)
+            {
+                if( diferidos[i] ){
+                    diferidos[i] = false;
+                    MPI_Send(NULL, 0, MPI_INT, 2*i, TAG_REPLY, COMM_WORLD);   
+                }
+            }
         }
         
         else if (tag == TAG_TERMINE) {
@@ -39,7 +53,22 @@ void servidor(int mi_cliente)
             debug("Mi cliente avisa que terminó");
             listo_para_salir = TRUE;
         }
-        
+
+        else if (tag == TAG_REQUEST) {
+            if( hay_pedido_local && mi_rank < origen){
+                diferidos[origen/2] = true;
+            } else {
+                MPI_Send(NULL, 0, MPI_INT, 2*i, TAG_REPLY, COMM_WORLD);
+            }
+        }
+
+        else if (tag == TAG_REPLY) {
+            respuestas_faltantes--;
+            if(respuestas_faltantes == 0){
+                respuestas_faltantes = 0;
+                MPI_Send(NULL, 0, MPI_INT, mi_cliente, TAG_OTORGADO, COMM_WORLD);
+            }
+        }
     }
     
 }
--- tp3/src/srv.h
+++ tp3/src/srv.h
@@ -3,6 +3,9 @@
 
 #include "tp3.h"
 
+#define TAG_REQUEST 60; /* srv0 -> srv1 */
+#define TAG_REPLY	70; /* srv1 -> srv0 */
+ 
 void servidor(int mi_cliente);
 
 #endif
