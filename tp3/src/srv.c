b1c1afd16482f4f6ed0c17951c817925ae0b0e3e
gbytz <gbytz.zyt@gmail.com>
2015-06-24 19:46:37 -0300

    Agrego comentarios

--- tp3/src/srv.c
+++ tp3/src/srv.c
@@ -1,11 +1,5 @@
 #include "srv.h"
 
-/*
- *  Ejemplo de servidor que tiene el "sí fácil" para con su
- *  cliente y no se lleva bien con los demás servidores.
- *
- */
-
 void debug_server(const char *mje, int origen) {
 #ifndef NDEBUG
     printf("\trk%-3d %c%-3d %c    %-50s rk%-3d s%-3d\n",
@@ -28,10 +22,11 @@ void servidor(int mi_cliente)
     int activos[cant_ranks/2];
     int servers_activos = cant_ranks/2;
 
-    // Contador de respuestas que me falta recibir para obtener acceso exclusivo
+    // Contador de respuestas que me falta recibir y de quien para obtener acceso exclusivo
     int respuestas_faltantes = (cant_ranks/2);
     int espero_respuesta[cant_ranks/2];
 
+    // Inicializo los arreglos: todos los servers estan activos, no espero respuestas y no diferi ninguna respuesta.
     int k;
     for(k = 0; k < cant_ranks/2; ++k){
         diferidos[k] = FALSE;
@@ -56,11 +51,13 @@ void servidor(int mi_cliente)
             hay_pedido_local = TRUE;
 
             if ( cant_ranks == 2 || servers_activos == 1 ) {
+                // Si soy el único server, le doy permiso a mi cliente
                 debug("Dejo pasar a mi cliente");
                 MPI_Send(NULL, 0, MPI_INT, mi_cliente, TAG_OTORGADO, COMM_WORLD);
             } else {
                 our_sequence_number = highest_sequence_number + 1;
                 respuestas_faltantes = 0;
+                // Envio un request solo a los servidores activos
                 int i;
                 for(i = 0; i < cant_ranks/2; ++i){
                     if( activos[i] ){
@@ -78,6 +75,7 @@ void servidor(int mi_cliente)
             debug("Mi cliente libera su acceso exclusivo");
             assert(hay_pedido_local == TRUE);
             hay_pedido_local = FALSE;
+            // Respondo a todos los servidores a quienes les pospuse la respuesta.
             int i;
             for(i = 0; i < cant_ranks/2; ++i)
             {
@@ -91,6 +89,7 @@ void servidor(int mi_cliente)
         else if (tag == TAG_TERMINE) {
             assert(origen == mi_cliente);
             debug("Mi cliente avisa que terminó");
+            // Aviso a todos los otros servidores activos que me voy a cerrar
             int i;
             for(i = 0; i < cant_ranks/2; ++i){
                 if( activos[i] ){
@@ -102,12 +101,15 @@ void servidor(int mi_cliente)
         }
 
         else if (tag == TAG_REQUEST) {
+            // Calculo el nuevo highest sequecne number
             highest_sequence_number = highest_sequence_number > their_sequence_number ? highest_sequence_number : their_sequence_number;
 
             if( hay_pedido_local && (mi_rank < origen || our_sequence_number < their_sequence_number) ){
+                // Si yo tenia un pedido de mi cliente y tengo prioridad, difiero la respuesta.
                 debug_server("Pospongo pedido de", origen);
                 diferidos[origen/2] = TRUE;
             } else {
+                // Si mi cliente no hizo un pedido de acceso exclusivo o si mi prioridad es menor.
                 debug_server("Le respondo a", origen);
                 MPI_Isend(NULL, 0, MPI_INT, origen, TAG_REPLY, COMM_WORLD, &request);
             }
@@ -128,6 +130,7 @@ void servidor(int mi_cliente)
         else if (tag == TAG_ME_CIERRO ){
             debug_server("Me avisa que se cierra", origen);
             if( espero_respuesta[origen/2] ){
+                // Si estaba esperando respuesta de un servidor pero primero me llega el mensaje de que se cierra, no espero más su respuesta.
                 espero_respuesta[origen/2] = FALSE;
                 respuestas_faltantes--;
             }
